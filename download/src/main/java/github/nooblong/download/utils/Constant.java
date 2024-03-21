c7dc19652503eac65f1538487bf677b87296c1c0
龙打野 <18128966990@163.com>
2024-03-21 15:55:30 +0800

    fix: create folder

--- download/src/main/java/github/nooblong/download/utils/Constant.java
+++ download/src/main/java/github/nooblong/download/utils/Constant.java
@@ -1,5 +1,14 @@
 package github.nooblong.download.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Slf4j
 public class Constant {
     public static final String BILI_API_URL = "localhost";
     public static final int BILI_API_PORT = 9000;
@@ -10,5 +19,20 @@ public class Constant {
     public static final String FFMPEG_FORMAT_M4A = "m4a";
     public static final int SEARCH_PAGE_SIZE = 30;
     public static final int MAX_RETRY_TIMES = 5;
-    public static final String TMP_FOLDER = System.getProperty("java.io.tmpdir");
+    public static String TMP_FOLDER;
+
+    static {
+        String tempDir = System.getProperty("java.io.tmpdir");
+        Path path = Paths.get(tempDir, "btncm");
+        try {
+            Path newDir = Files.createDirectory(path);
+            log.info("文件夹创建成功：" + newDir);
+        } catch (FileAlreadyExistsException e) {
+            log.info("文件夹已经存在: " + path);
+        } catch (IOException e) {
+            log.error("无法创建文件夹: " + path);
+            throw new RuntimeException(e);
+        }
+        TMP_FOLDER = path.toAbsolutePath().toString();
+    }
 }
--- download/src/test/java/github/nooblong/download/service/impl/FfmpegServiceImplTest.java
+++ download/src/test/java/github/nooblong/download/service/impl/FfmpegServiceImplTest.java
@@ -2,12 +2,19 @@ package github.nooblong.download.service.impl;
 
 import github.nooblong.download.BaseTest;
 import github.nooblong.download.service.FfmpegService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import ws.schild.jave.info.MultimediaInfo;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+@Slf4j
 class FfmpegServiceImplTest extends BaseTest {
 
     @Autowired
@@ -23,6 +30,15 @@ class FfmpegServiceImplTest extends BaseTest {
     @Test
     void getTmpFolder() {
         String tempDir = System.getProperty("java.io.tmpdir");
-        System.out.println("临时文件夹路径：" + tempDir);
+        Path path = Paths.get(tempDir, "btncm");
+        try {
+            Path newDir = Files.createDirectory(path);
+            log.info("文件夹创建成功：" + newDir);
+        } catch (FileAlreadyExistsException e) {
+            log.info("文件夹已经存在: " + path);
+        } catch (IOException e) {
+            log.error("无法创建文件夹: " + path);
+            throw new RuntimeException(e);
+        }
     }
 }
