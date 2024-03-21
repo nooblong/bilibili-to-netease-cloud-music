35b5ad54eb19e1c114726d1bf6c2103f30ba3169
龙打野 <18128966990@163.com>
2024-03-21 16:45:28 +0800

    Update FfmpegServiceImpl.java

--- download/src/main/java/github/nooblong/download/service/impl/FfmpegServiceImpl.java
+++ download/src/main/java/github/nooblong/download/service/impl/FfmpegServiceImpl.java
@@ -50,12 +50,12 @@ public class FfmpegServiceImpl implements FfmpegService, InitializingBean {
         AudioAttributes audioAttributes = new AudioAttributes();
         // 设置编码过程的音量值。如果为 null 或未指定，则将选择默认值。如果是 256，则不会执行任何音量更改。
         // 音量是“振幅比”或“声压级”比率 2560 是音量=20dB 公式是 dBnumber=20*lg(振幅比) 128 表示减小 50% 512 表示音量加倍
-        audioAttributes.setVolume(voiceOffset);
+        audioAttributes.setVolume(voiceOffset == 0 ? null : voiceOffset);
         EncodingAttributes encodingAttributes = new EncodingAttributes();
         encodingAttributes.setAudioAttributes(audioAttributes);
         encodingAttributes.setOutputFormat(ext);
-        encodingAttributes.setDuration((float) duration);
-        encodingAttributes.setOffset((float) beginSec);
+        encodingAttributes.setDuration(((float) duration) == 0 ? null : (float) duration);
+        encodingAttributes.setOffset(((float) beginSec) == 0 ? null : (float) beginSec);
 
         VideoAttributes videoAttributes = new VideoAttributes();
         encodingAttributes.setVideoAttributes(videoAttributes);
