acd194ca987c14577b6804395deca196a2ed3110
龙打野 <18128966990@163.com>
2024-03-29 10:02:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 cd /root/pub
 
-kill $(ps aux | grep java | grep -v 'grep' | awk '{print $2}')
+kill $(ps aux | grep java.*system | grep -v 'grep' | awk '{print $2}')
 
 kill $(ps aux | grep python | grep -v 'grep' | awk '{print $2}')
 
@@ -12,9 +12,9 @@ rm -f /root/spring.txt
 
 source /etc/profile
 
-nohup java -jar /root/powerjob-server-starter-4.3.9.jar > /dev/null 2>&1 &
+#nohup java -jar /root/powerjob-server-starter-4.3.9.jar > /dev/null 2>&1 &
 
-sleep 30s
+#sleep 30s
 
 nohup java -jar system-1.0-SNAPSHOT.jar > /root/spring.txt 2>&1 &
 
