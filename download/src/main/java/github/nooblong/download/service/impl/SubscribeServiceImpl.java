e21fa69a0795a53ba80f02978a85ab5016ecd2e8
nooblong <18128966990@163.com>
2025-04-23 18:59:30 +0800

    fix: log

--- download/src/main/java/github/nooblong/download/service/impl/SubscribeServiceImpl.java
+++ download/src/main/java/github/nooblong/download/service/impl/SubscribeServiceImpl.java
@@ -142,7 +142,7 @@ public class SubscribeServiceImpl extends ServiceImpl<SubscribeMapper, Subscribe
             log.error("订阅: {} 处理失败: {}", subscribe.getId(), e.getMessage());
             log.error(e.getMessage(), e);
             subscribe.setLog(CommonUtil.processString(subscribe.getLog()) + DateUtil.now() +
-                    " 订阅处理失败，原因: " + CommonUtil.limitString(e.getMessage()) + "\n");
+                    " 订阅处理失败，原因: " + e.getMessage() + "\n");
             updateById(subscribe);
         }
     }
